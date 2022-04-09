b1b59ce8db33b63b5ad6758e13bdecdc81f699c5
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2022-04-08 22:50:45 -0500

    Spring con JWT Basic

---
+++ src/main/java/com/idat/idatapirest/dto/UserResponseDTO.java
@@ -0,0 +1,23 @@
+package com.idat.idatapirest.dto;
+
+public class UserResponseDTO {
+	
+	private String token;
+
+	
+	public UserResponseDTO(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+
+}
